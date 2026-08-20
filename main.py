35d51d4cb1b6fb9269d15285bfe0fcd31dfa5f14
goshanathan64-ops <goshanathan64@gmail.com>
2026-08-20 19:04:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ MDScreen:
     MDBottomNavigation:
         panel_color: "#0066FF"
         text_color_active: "white"
+        text_color_normal: "#B3D1FF"
         
         MDBottomNavigationItem:
             name: 'home'
@@ -16,12 +17,33 @@ MDScreen:
             MDLabel:
                 text: 'TUMA Super App V23.3'
                 halign: 'center'
-                font_style: 'H3'  # CHANGED FROM H4
+                font_style: 'H3'
+        
+        MDBottomNavigationItem:
+            name: 'wallet'
+            text: 'Wallet'
+            icon: 'wallet'
+            MDLabel:
+                text: 'Wallet Coming Soon'
+                halign: 'center'
+                font_style: 'H3'
+        
+        MDBottomNavigationItem:
+            name: 'profile'
+            text: 'Profile'
+            icon: 'account'
+            MDLabel:
+                text: 'Profile Coming Soon'
+                halign: 'center'
+                font_style: 'H3'
 '''
 
 class TumaApp(MDApp):
     def build(self):
+        self.title = "TUMA"
         self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.theme_style = "Light"
         return Builder.load_string(KV)
 
-TumaApp().run()
+if __name__ == '__main__':
+    TumaApp().run()
