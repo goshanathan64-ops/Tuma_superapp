51491d7770f59d286fcea2c0dfc37b9d73b4caed
goshanathan64-ops <goshanathan64@gmail.com>
2026-08-22 14:45:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ class TumaApp(MDApp):
         self.title = "TUMA V23.3"
         self.theme_cls.primary_palette = "Blue"
         screen = MDScreen()
-        screen.add_widget(
-            MDLabel(text="TUMA Super App V23.3", halign="center", font_style="H3")
-        )
+        screen.add_widget(MDLabel(text="TUMA Super App V23.3", halign="center", font_style="H3"))
         return screen
 
-TumaApp().run()
+if __name__ == '__main__':
+    TumaApp().run()
